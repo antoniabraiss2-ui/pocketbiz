91fe6464f51a1b485af5762f83eb6e134d33c91b
antoniabraiss2-ui <antoniabraiss2@gmail.com>
2026-09-21 21:59:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,18 @@
-const CACHE_NAME = 'pocketbiz-v5';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.svg'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((k) => { if (k !== CACHE_NAME) return caches.delete(k); })
-      );
+      return Promise.all(keys.map((k) => caches.delete(k)));
     })
   );
-  self.clients.claim();
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
